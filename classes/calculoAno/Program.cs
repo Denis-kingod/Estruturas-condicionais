10315e226947a2ccc74fb8cdb1ea6763f8824da7
Denis <denis.alexandre200295@gmail.com>
2021-06-30 23:28:31 -0300

    estruturas condicionais completo

---
+++ classes/calculoAno/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace calculoAno
+{
+    class Program
+    {
+        public static int idadeSemana;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calculo da idade \n");
+            Console.WriteLine("qual sua idade atual?");
+            int anoAtual = int.Parse(Console.ReadLine());
+
+
+            int idade = (int)(idadeSemana *52.143);
+            Console.WriteLine("sua idade em semanas é:");
+        }
+    }
+}
---
+++ classes/nadadores/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace nadadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Para começarmos coloque a sua idade!");
+            int idade = int.Parse(Console.ReadLine());
+
+             if (idade > 18)
+            {
+                Console.WriteLine("Nadadores senior");
+            }else {
+                if (idade > 16)
+                {
+                    Console.WriteLine("Juvenil B");
+                }else {
+                    if (idade > 12 )
+                    {
+                        Console.WriteLine("Juvenil A");
+                    }else {
+                        if (idade > 7)
+                        {
+                            Console.WriteLine("Infantil B");
+                        }else {
+                            if (idade > 4)
+                            {
+                                Console.WriteLine("Infantil A");
+                            }else {
+                                Console.WriteLine("Infelizmente só permitimos a idade a partir de 5 anos...");
+                            }                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
